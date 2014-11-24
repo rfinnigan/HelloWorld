a4fbf09340d6795961080e42efd4f155990143c0
Ruairidh Finnigan <ruairidhfinnigan@gmail.com>
2014-11-24 22:11:37 +0000

    Fix errors in code

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		new String hiThere = "Hello World"
+		String hiThere = "Hello World";
 		System.out.println(hiThere);
 
 	}
